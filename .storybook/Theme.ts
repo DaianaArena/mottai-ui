9cc55ba76bc08da11f32ddd4dc462132eb77afb2
Daiana <arenadaianam@gmail.com>
2025-05-21 00:50:02 -0300

    Add Storybook theme configuration and manager setup

---
+++ .storybook/Theme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+//TODO: Add a logo and url for the landing page
+export default create({
+  base: 'dark',
+  brandTitle: 'Mottai UI',
+  brandUrl: 'https://example.com',
+  brandImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcvYCiCzOFtb5TScbaGKVIDOHznHJCtiZ36w&s',
+  brandTarget: '_self',
+});
