3338b6fd53a37f81bac69cba627c2fdf121493f3
Daiana <arenadaianam@gmail.com>
2025-05-21 00:23:12 -0300

    Update package dependencies, enhance build scripts with Tailwind CSS integration, and apply Tailwind styles to Card component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -5,13 +5,15 @@ type CardProps = {
   
   export const Card = ({ title, description }: CardProps) => {
     return (
-      <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-white">
-        <div className="px-6 py-4">
-          <div className="space-y-2">
-            <h2 className="text-xl font-bold text-gray-800">{title}</h2>
-            <p className="text-gray-600 text-base">{description}</p>
+      <div className="mottai">
+      <div className="mottai-max-w-sm mottai-rounded mottai-overflow-hidden mottai-shadow-lg mottai-m-4 mottai-bg-white">
+        <div className="mottai-px-6 mottai-py-4">
+          <div className="mottai-space-y-2">
+            <h2 className="mottai-text-xl mottai-font-bold mottai-text-gray-800">{title}</h2>
+            <p className="mottai-text-gray-600 mottai-text-base">{description}</p>
           </div>
         </div>
+      </div>
       </div>
     );
   };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: '.mottai',
+  prefix: 'mottai-',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
